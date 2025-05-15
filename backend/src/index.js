fd64fbb5eac165fca910d409ec012987161090cf
Ayush Pratap <ayushprince110@gmail.com>
2025-05-15 15:28:08 +0530

    Profile page

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,9 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(cookieParser());
 app.use(
   cors({
